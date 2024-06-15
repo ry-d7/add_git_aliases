f8d94c5c337a55e49874078e8aa7e2f021db2f13
ryo yoshimura <59388899+ry-d7@users.noreply.github.com>
2024-06-15 21:22:34 +0900

    add the script file

---
+++ add_git_aliases.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+
+declare -a ALIASKEYS
+ALIASKEYS=("status"
+          "checkout"
+          "diff"
+          "diff_cached"
+          "log"
+          "log_p"
+          "commit"
+          "commit_m"
+          "merge"
+          "tag"
+          )
+
+declare -A ALIASES
+ALIASES[status,command]="status"
+ALIASES[status,short]="s"
+
+ALIASES[checkout,command]="checkout"
+ALIASES[checkout,short]="co"
+
+ALIASES[diff,command]="diff"
+ALIASES[diff,short]="df"
+
+ALIASES[diff_cached,command]="diff --cached"
+ALIASES[diff_cached,short]="dfc"
+
+ALIASES[log,command]="log"
+ALIASES[log,short]="l"
+
+ALIASES[log_p,command]="log -p"
+ALIASES[log_p,short]="lp"
+
+ALIASES[commit,command]="commit"
+ALIASES[commit,short]="cm"
+
+ALIASES[commit_m,command]="commit -m"
+ALIASES[commit_m,short]="cmm"
+
+ALIASES[merge,command]="merge"
+ALIASES[merge,short]="m"
+
+ALIASES[tag,command]="tag"
+ALIASES[tag,short]="t"
+
+for key in "${ALIASKEYS[@]}"; do
+    command="${ALIASES[${key},command]}"
+    short="${ALIASES[${key},short]}"
+    git config --global --get alias.${short} > /dev/null 2>&1
+    if [ $? = 0 ]; then
+        echo "git alias ${short} already exists"
+    else
+        echo "add git alias ${short}"
+        git config --global alias.${short} ${command}
+    fi
+done
